f604c426e58f200349c2246d51c4e962bba9f3e4
Ethan Manchon <ethan.manchon@etu.unilim.fr>
2025-01-29 17:57:41 +0100

    Nouvelle version du projet

--- config/bundles.php
+++ config/bundles.php
@@ -4,7 +4,10 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+<<<<<<< HEAD
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+=======
+>>>>>>> e52e564 (Nouvelle version du projet)
 ];
